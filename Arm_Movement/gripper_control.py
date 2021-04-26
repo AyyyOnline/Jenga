ecf4d247a892cf07967efe459a2127044a09c802
Andre <49556773+AyyyOnline@users.noreply.github.com>
2021-04-26 05:33:48 +0100

    Update gripper_control.py

--- Arm_Movement/gripper_control.py
+++ Arm_Movement/gripper_control.py
@@ -1,7 +1,6 @@
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 # This script closes and opens the gripper twice, changing the gripper pressure half way through
-#
-# To get started, open a terminal and type 'roslaunch interbotix_xsarm_control xsarm_control.launch robot_model:=wx200'
+# roslaunch interbotix_xsarm_control xsarm_control.launch robot_model:=rx150 in a terminal
 # Then change to this directory and type 'python gripper_control.py'
 
 def main():
